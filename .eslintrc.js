faad0f72cfe5217fc4bf1653fda97259ea4bc540
Nikolay Rademacher <hrrdx33@gmail.com>
2021-03-30 17:06:37 +0200

    add eslint setup script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {
-  },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
