6c44b3179cb2490560bb21b3a40e4f3179ca618a
Nikolay Rademacher <hrrdx33@gmail.com>
2021-03-30 03:28:02 +0200

    optimize react prod bundle size

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looks like we are in development mode!");
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.dev.js
+++ webpack.dev.js
@@ -7,4 +7,7 @@ module.exports = merge(common, {
   devServer: {
     contentBase: "./dist",
   },
+  optimization: {
+    usedExports: true,
+  },
 });
