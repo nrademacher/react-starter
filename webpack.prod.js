eb2a9c9968cb0f31b892a29b649419d7cca0703a
Nikolay Rademacher <hrrdx33@gmail.com>
2021-04-04 19:37:07 +0200

    switch from babel-loader to esbuild-loader (reason: much faster)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,22 @@
-const { DefinePlugin } = require("webpack");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const { DefinePlugin } = require("webpack");
+const { ESBuildMinifyPlugin } = require("esbuild-loader");
 
 module.exports = merge(common, {
   mode: "production",
-  // devtool: "source-map",
+  // devtool: "source-map", // Uncomment for source maps in production
   plugins: [
     new DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production"),
     }),
   ],
+  optimization: {
+    minimizer: [
+      new ESBuildMinifyPlugin({
+        target: "es2015",
+      }),
+    ],
+  },
 });
+
