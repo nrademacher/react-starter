5656f6f6570b7865ea9f03dae40f62eee21f7f59
Nikolay Rademacher <47133234+cxxiii@users.noreply.github.com>
2021-04-22 17:00:55 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: "jit",
-  purge: ["./src/**/*.html", "./src/index.html", "./src/**/*.js"],
+  purge: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
