8f42d78f6642c6bea6c846693664fdfbf4e5a483
Nikolay Rademacher <hrrdx33@gmail.com>
2021-05-17 19:12:23 +0200

    feat: from tailwind to windi (fewer deps, streamlining)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
+import WindiCSS from 'vite-plugin-windicss';
 
 export default defineConfig({
-  plugins: [reactRefresh(),],
+  plugins: [reactRefresh(), WindiCSS()],
 });
