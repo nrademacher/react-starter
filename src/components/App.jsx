98aa199cde2cb908d8a2f65ef9347a99aa2626c5
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2021-05-21 18:58:51 +0200

    fix: class to className

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => (
-  <h1 class="grid place-items-center min-h-screen text-8xl font-light text-indigo-800">
+  <h1 className="grid place-items-center min-h-screen text-8xl font-light text-indigo-800">
     Hello, world.
   </h1>
 );
